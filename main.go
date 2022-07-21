9c3bc1517b84ddc1e30c2930292db8b801cef534
Sebastian Hoß <seb@ho.de>
2022-07-21 08:34:36 +0200

    support debuggers like delve

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
@@ -13,8 +14,14 @@ import (
 //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 
 func main() {
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
 	opts := providerserver.ServeOpts{
 		Address: "registry.terraform.io/metio/git",
+		Debug:   debug,
 	}
 
 	err := providerserver.Serve(context.Background(), provider.New, opts)
