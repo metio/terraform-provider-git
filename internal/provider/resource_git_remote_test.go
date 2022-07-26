cbcce7b9f3007661f5124f1287e472bf5d8b4ce0
Sebastian Hoß <seb@ho.de>
2022-07-26 07:53:52 +0200

    more tests for git_remote resource

--- internal/provider/resource_git_remote_test.go
+++ internal/provider/resource_git_remote_test.go
@@ -11,6 +11,7 @@ import (
 	"fmt"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"os"
+	"regexp"
 	"testing"
 )
 
@@ -37,6 +38,118 @@ func TestResourceGitRemote(t *testing.T) {
 					resource.TestCheckResourceAttr("git_remote.test", "name", name),
 					resource.TestCheckResourceAttr("git_remote.test", "urls.#", "1"),
 				),
+			},
+		},
+	})
+}
+
+func TestResourceGitRemote_MultipleUrls(t *testing.T) {
+	t.Parallel()
+	directory, _ := initializeGitRepository(t)
+	defer os.RemoveAll(directory)
+	name := "some-name"
+
+	resource.UnitTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: protoV6ProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+					resource "git_remote" "test" {
+						directory = "%s"
+						name      = "%s"
+						urls      = ["https://github.com/some-org/some-repo.git", "https://gitlab.com/some-org/some-repo.git"]
+					}
+				`, directory, name),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("git_remote.test", "directory", directory),
+					resource.TestCheckResourceAttr("git_remote.test", "id", directory),
+					resource.TestCheckResourceAttr("git_remote.test", "name", name),
+					resource.TestCheckResourceAttr("git_remote.test", "urls.#", "2"),
+				),
+			},
+		},
+	})
+}
+
+func TestResourceGitRemote_InvalidRepository(t *testing.T) {
+	t.Parallel()
+	name := "some-name"
+
+	resource.UnitTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: protoV6ProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+					resource "git_remote" "test" {
+						directory = "/some/random/path"
+						name      = "%s"
+						urls      = ["https://github.com/some-org/some-repo.git", "https://gitlab.com/some-org/some-repo.git"]
+					}
+				`, name),
+				ExpectError: regexp.MustCompile(`Cannot open repository`),
+			},
+		},
+	})
+}
+
+func TestResourceGitRemote_MissingRepository(t *testing.T) {
+	t.Parallel()
+	name := "some-name"
+
+	resource.UnitTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: protoV6ProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+					resource "git_remote" "test" {
+						name      = "%s"
+						urls      = ["https://github.com/some-org/some-repo.git", "https://gitlab.com/some-org/some-repo.git"]
+					}
+				`, name),
+				ExpectError: regexp.MustCompile(`Missing required argument`),
+			},
+		},
+	})
+}
+
+func TestResourceGitRemote_MissingName(t *testing.T) {
+	t.Parallel()
+	directory, _ := initializeGitRepository(t)
+	defer os.RemoveAll(directory)
+
+	resource.UnitTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: protoV6ProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+					resource "git_remote" "test" {
+						directory = "%s"
+						urls      = ["https://github.com/some-org/some-repo.git", "https://gitlab.com/some-org/some-repo.git"]
+					}
+				`, directory),
+				ExpectError: regexp.MustCompile(`Missing required argument`),
+			},
+		},
+	})
+}
+
+func TestResourceGitRemote_MissingUrls(t *testing.T) {
+	t.Parallel()
+	directory, _ := initializeGitRepository(t)
+	defer os.RemoveAll(directory)
+	name := "some-name"
+
+	resource.UnitTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: protoV6ProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+					resource "git_remote" "test" {
+						directory = "%s"
+						name      = "%s"
+					}
+				`, directory, name),
+				ExpectError: regexp.MustCompile(`Missing required argument`),
 			},
 		},
 	})
