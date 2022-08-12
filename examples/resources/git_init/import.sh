bc0b7212022b03b4c4e79f56a79b6169392a5026
Sebastian Hoß <seb@ho.de>
2022-08-12 16:47:38 +0200

    remove unnecessary license infos b/c they are in dep5

--- examples/resources/git_init/import.sh
+++ examples/resources/git_init/import.sh
@@ -1,7 +1,2 @@
-#
-# SPDX-FileCopyrightText: Sebastian Hoß <seb@hoß.de>
-# SPDX-License-Identifier: 0BSD
-#
-
 # git_init resources can be imported by specifying the path to the Git repository.
 terraform import git_init.init 'path/to/your/git/repository'
--- examples/resources/git_remote/import.sh
+++ examples/resources/git_remote/import.sh
@@ -1,8 +1,3 @@
-#
-# SPDX-FileCopyrightText: Sebastian Hoß <seb@hoß.de>
-# SPDX-License-Identifier: 0BSD
-#
-
 # git_remote resources can be imported by specifying the directory of the Git repository and the name of the remote to
 # import. Both values are separated by a single '|'.
 terraform import git_remote.remote 'path/to/your/git/repository|name-of-your-remote'
