e5f347d34dbf21c009817cef77d6de3eca2ab5c3
Sebastian Hoß <seb@ho.de>
2022-08-12 16:00:13 +0200

    change license to BSD0

--- examples/resources/git_remote/import.sh
+++ examples/resources/git_remote/import.sh
@@ -1,3 +1,8 @@
+#
+# SPDX-FileCopyrightText: Sebastian Hoß <seb@hoß.de>
+# SPDX-License-Identifier: BSD0
+#
+
 # git_remote resources can be imported by specifying the directory of the Git repository and the name of the remote to
 # import. Both values are separated by a single '|'.
 terraform import git_remote.remote 'path/to/your/git/repository|name-of-your-remote'
