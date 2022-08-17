c46b3b6764925f839b4af8dccdaa53640c1a7fa7
Sebastian Hoß <seb@ho.de>
2022-08-17 08:18:13 +0200

    protect against missing author/committer infos

--- internal/provider/data_source_git_commit_test.go
+++ internal/provider/data_source_git_commit_test.go
@@ -7,6 +7,7 @@ package provider_test
 
 import (
 	"fmt"
+	"github.com/go-git/go-git/v5"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"os"
 	"testing"
@@ -45,6 +46,48 @@ func TestDataSourceGitCommit(t *testing.T) {
 					resource.TestCheckResourceAttrWith("data.git_commit.test", "author.timestamp", testCheckMinLength(1)),
 					resource.TestCheckResourceAttrWith("data.git_commit.test", "committer.name", testCheckMinLength(1)),
 					resource.TestCheckResourceAttrWith("data.git_commit.test", "committer.email", testCheckMinLength(1)),
+					resource.TestCheckResourceAttrWith("data.git_commit.test", "committer.timestamp", testCheckMinLength(1)),
+				),
+			},
+		},
+	})
+}
+
+func TestDataSourceGitCommit_WithSignature(t *testing.T) {
+	t.Parallel()
+	directory, repository := testRepository(t)
+	defer os.RemoveAll(directory)
+	worktree := testWorktree(t, repository)
+	fileName := "some-file"
+	testWriteFile(t, worktree, fileName)
+	testGitAdd(t, worktree, fileName)
+	signature := testSignature()
+	commit := testGitCommitWith(t, worktree, &git.CommitOptions{
+		Author: signature,
+	})
+
+	resource.UnitTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: testProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+					data "git_commit" "test" {
+						directory = "%s"
+						sha1      = "%s"
+					}
+				`, directory, commit.String()),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.git_commit.test", "directory", directory),
+					resource.TestCheckResourceAttr("data.git_commit.test", "id", commit.String()),
+					resource.TestCheckResourceAttr("data.git_commit.test", "sha1", commit.String()),
+					resource.TestCheckResourceAttr("data.git_commit.test", "signature", ""),
+					resource.TestCheckResourceAttrWith("data.git_commit.test", "message", testCheckMinLength(1)),
+					resource.TestCheckResourceAttrWith("data.git_commit.test", "tree_sha1", testCheckMinLength(1)),
+					resource.TestCheckResourceAttr("data.git_commit.test", "author.name", signature.Name),
+					resource.TestCheckResourceAttr("data.git_commit.test", "author.email", signature.Email),
+					resource.TestCheckResourceAttrWith("data.git_commit.test", "author.timestamp", testCheckMinLength(1)),
+					resource.TestCheckResourceAttr("data.git_commit.test", "committer.name", signature.Name),
+					resource.TestCheckResourceAttr("data.git_commit.test", "committer.email", signature.Email),
 					resource.TestCheckResourceAttrWith("data.git_commit.test", "committer.timestamp", testCheckMinLength(1)),
 				),
 			},
