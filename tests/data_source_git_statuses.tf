13fef87c8a492e375013b0756cbe4f4181ef5f81
Sebastian Hoß <seb@ho.de>
2022-08-20 13:57:10 +0200

    add all remaining data sources

--- tests/data_source_git_statuses.tf
+++ tests/data_source_git_statuses.tf
@@ -1,10 +1,10 @@
 # SPDX-FileCopyrightText: The terraform-provider-git Authors
 # SPDX-License-Identifier: 0BSD
 
-data "git_tags" "all" {
+data "git_statuses" "statuses" {
   directory = data.git_repository.repository.directory
 }
 
-output "data_source_git_tags_all" {
-  value = data.git_tags.all
+output "data_source_git_statuses_statuses" {
+  value = data.git_statuses.statuses
 }
--- tests/data_source_git_tag.tf
+++ tests/data_source_git_tag.tf
@@ -1,10 +1,12 @@
 # SPDX-FileCopyrightText: The terraform-provider-git Authors
 # SPDX-License-Identifier: 0BSD
 
-data "git_tags" "all" {
+data "git_tag" "every" {
+  for_each  = data.git_tags.all.tags
   directory = data.git_repository.repository.directory
+  name      = each.key
 }
 
-output "data_source_git_tags_all" {
-  value = data.git_tags.all
+output "data_source_git_tag_every" {
+  value = data.git_tag.every[*]
 }
