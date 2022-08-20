f7334ad9b7fb36ec5859ad9e8f58ae9cf3453cd4
Sebastian Hoß <seb@ho.de>
2022-08-20 12:14:21 +0200

    add missing license headers

--- tests/variables.tf
+++ tests/variables.tf
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: The terraform-provider-git Authors
+# SPDX-License-Identifier: 0BSD
+
 variable "git_repo_path" {
   type        = string
   description = "The root directory of a Git repository."
