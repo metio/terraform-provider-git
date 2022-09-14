28d7fb9524eea70f896d8dc9631ce763400ce881
Sebastian Hoß <seb@ho.de>
2022-09-14 13:26:13 +0200

    improve import state tests for git_init

--- internal/provider/resource_init_test.go
+++ internal/provider/resource_init_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/metio/terraform-provider-git/internal/testutils"
 	"os"
+	"regexp"
 	"testing"
 )
 
@@ -87,7 +88,7 @@ func TestResourceGitInit_NonBare(t *testing.T) {
 	})
 }
 
-func TestResourceGitInit_Import(t *testing.T) {
+func TestResourceGitInit_Import_NonExistingRepo(t *testing.T) {
 	t.Parallel()
 	directory := testutils.TemporaryDirectory(t)
 	defer os.RemoveAll(directory)
@@ -101,17 +102,51 @@ func TestResourceGitInit_Import(t *testing.T) {
 						directory = "%s"
 					}
 				`, directory),
+				ResourceName:       "git_init.test",
+				ImportState:        true,
+				ImportStateId:      directory,
+				ImportStatePersist: false,
+				ExpectError:        regexp.MustCompile(`Cannot open repository`),
+			},
+		},
+	})
+}
+
+func TestResourceGitInit_Import_ExistingRepo(t *testing.T) {
+	t.Parallel()
+	directory, _ := testutils.CreateRepository(t)
+	defer os.RemoveAll(directory)
+
+	resource.UnitTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: testutils.ProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+					resource "git_init" "test" {
+						directory = "%s"
+					}
+				`, directory),
+				ResourceName:       "git_init.test",
+				ImportState:        true,
+				ImportStateId:      directory,
+				ImportStatePersist: true,
+				ImportStateCheck: testutils.ComposeImportStateCheck(
+					testutils.CheckResourceAttrInstanceState("directory", directory),
+					testutils.CheckResourceAttrInstanceState("id", directory),
+					testutils.CheckResourceAttrInstanceState("bare", "false"),
+				),
+			},
+			{
+				Config: fmt.Sprintf(`
+					resource "git_init" "test" {
+						directory = "%s"
+					}
+				`, directory),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("git_init.test", "directory", directory),
 					resource.TestCheckResourceAttr("git_init.test", "id", directory),
 					resource.TestCheckResourceAttr("git_init.test", "bare", "false"),
 				),
-			},
-			{
-				ResourceName:      "git_init.test",
-				ImportState:       true,
-				ImportStateId:     directory,
-				ImportStateVerify: true,
 			},
 		},
 	})
--- internal/testutils/checkers.go
+++ internal/testutils/checkers.go
@@ -9,6 +9,8 @@ package testutils
 
 import (
 	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 func CheckExactLength(expectedLength int) func(input string) error {
@@ -25,6 +27,39 @@ func CheckMinLength(minimumLength int) func(input string) error {
 	return func(input string) error {
 		if len(input) < minimumLength {
 			return fmt.Errorf("minimum length %d, actual length %d", minimumLength, len(input))
+		}
+
+		return nil
+	}
+}
+
+func ComposeImportStateCheck(fs ...resource.ImportStateCheckFunc) resource.ImportStateCheckFunc {
+	return func(s []*terraform.InstanceState) error {
+		for i, f := range fs {
+			if err := f(s); err != nil {
+				return fmt.Errorf("check %d/%d error: %s", i+1, len(fs), err)
+			}
+		}
+
+		return nil
+	}
+}
+
+func CheckResourceAttrInstanceState(attributeName, attributeValue string) resource.ImportStateCheckFunc {
+	return func(is []*terraform.InstanceState) error {
+		if len(is) != 1 {
+			return fmt.Errorf("unexpected number of instance states: %d", len(is))
+		}
+
+		s := is[0]
+
+		attrVal, ok := s.Attributes[attributeName]
+		if !ok {
+			return fmt.Errorf("attribute '%s' not found in instance state", attributeName)
+		}
+
+		if attrVal != attributeValue {
+			return fmt.Errorf("attribute '%s' expected: '%s', got: '%s'", attributeName, attributeValue, attrVal)
 		}
 
 		return nil
