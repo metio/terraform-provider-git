a51e34354edd612e8f626b5562ed722b7c0aa43c
Sebastian Hoß <seb@ho.de>
2022-09-17 14:45:09 +0200

    add test & example for repeated git_commit w/ git_add

--- terratest/tests/resource_git_commit_test.go
+++ terratest/tests/resource_git_commit_test.go
@@ -44,6 +44,51 @@ func TestResourceGitCommit_WithAdd(t *testing.T) {
 	assert.Equal(t, 1, len(actualFiles), "actualFiles")
 }
 
+func TestResourceGitCommit_WithAdd_Repeated(t *testing.T) {
+	directory, repository := testutils.CreateRepository(t)
+	defer os.RemoveAll(directory)
+	worktree := testutils.GetRepositoryWorktree(t, repository)
+	filename := "some-file"
+	testutils.WriteFileInWorktree(t, worktree, filename)
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../resources/git_commit/with_add",
+		Vars: map[string]interface{}{
+			"directory": directory,
+			"add_paths": []string{filename},
+			"file":      filename,
+		},
+		NoColor: true,
+	})
+
+	defer terraform.Destroy(t, terraformOptions)
+	terraform.InitAndApplyAndIdempotent(t, terraformOptions)
+
+	actualStaging := terraform.Output(t, terraformOptions, "staging")
+	actualWorktree := terraform.Output(t, terraformOptions, "worktree")
+	actualSha1 := terraform.Output(t, terraformOptions, "sha1")
+	actualFiles := terraform.OutputList(t, terraformOptions, "files")
+
+	assert.Equal(t, "?", actualStaging, "actualStaging")
+	assert.Equal(t, "?", actualWorktree, "actualWorktree")
+	assert.NotNil(t, actualSha1, "actualSha1")
+	assert.Equal(t, 1, len(actualFiles), "actualFiles")
+
+	testutils.WriteFileContent(t, testutils.FileInWorktree(worktree, filename), "new content")
+
+	terraform.ApplyAndIdempotent(t, terraformOptions)
+
+	actualStaging = terraform.Output(t, terraformOptions, "staging")
+	actualWorktree = terraform.Output(t, terraformOptions, "worktree")
+	actualSha1 = terraform.Output(t, terraformOptions, "sha1")
+	actualFiles = terraform.OutputList(t, terraformOptions, "files")
+
+	assert.Equal(t, "?", actualStaging, "actualStaging")
+	assert.Equal(t, "?", actualWorktree, "actualWorktree")
+	assert.NotNil(t, actualSha1, "actualSha1")
+	assert.Equal(t, 1, len(actualFiles), "actualFiles")
+}
+
 func TestResourceGitCommit_All(t *testing.T) {
 	directory, repository := testutils.CreateRepository(t)
 	defer os.RemoveAll(directory)
